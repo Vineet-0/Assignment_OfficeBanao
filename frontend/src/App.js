a8351868ae890b92e92e8c4728969f77a2ea07cf
Vineet-0 <vineet351016@gmail.com>
2024-03-02 19:07:40 +0530

    First Com

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline w-full min-h-screen flex items-center justify-center">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
